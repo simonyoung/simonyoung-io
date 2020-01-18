cb8932ca11f51594cf4e0387cf6c4049530266dd
Simon Young <simon.young@lego.com>
2020-01-18 22:32:34 +0000

    Add sample home and about routes

---
+++ pages/about.js
@@ -0,0 +1,5 @@
+function AboutPage() {
+  return <h1>About Simon</h1>
+}
+
+export default AboutPage
---
+++ pages/index.js
@@ -0,0 +1,5 @@
+function HomePage() {
+  return <h1>Simon Young</h1>
+}
+
+export default HomePage
