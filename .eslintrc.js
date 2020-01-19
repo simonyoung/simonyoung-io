47f688d9b342266abbb66581ddb2f08da66d1694
Simon Young <simon.young@lego.com>
2020-01-19 22:20:19 +0000

    Adding dev tools

---
+++ .eslintrc.js
@@ -0,0 +1,33 @@
+module.exports = {
+    'env': {
+        'browser': true,
+        'es6': true
+    },
+    'extends': [
+        'eslint:recommended',
+        'plugin:react/recommended',
+        'prettier',
+        'prettier/react'
+    ],
+    'globals': {
+      Promise: true
+    },
+    'plugins': [
+        'react',
+        'prettier',
+        'import'
+    ],
+    'rules': {
+      'prettier/prettier': 'error',
+      'react/react-in-jsx-scope': 'off',
+      'no-unused-vars': [
+        'error',
+        {
+          argsIgnorePattern: '^_',
+          varsIgnorePattern: '^_',
+          ignoreRestSiblings: true
+        }
+      ],
+      'no-param-reassign': 'error'
+    }
+};
