fe2ca5adfbce446e436d09c38f37cfd96baf87ef
Simon Young <simon.young@lego.com>
2020-01-19 22:24:40 +0000

    Fix linting on config files

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,33 +1,29 @@
 module.exports = {
-    'env': {
-        'browser': true,
-        'es6': true
-    },
-    'extends': [
-        'eslint:recommended',
-        'plugin:react/recommended',
-        'prettier',
-        'prettier/react'
+  env: {
+    browser: true,
+    es6: true
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'prettier',
+    'prettier/react'
+  ],
+  globals: {
+    Promise: true
+  },
+  plugins: ['react', 'prettier', 'import'],
+  rules: {
+    'prettier/prettier': 'error',
+    'react/react-in-jsx-scope': 'off',
+    'no-unused-vars': [
+      'error',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+        ignoreRestSiblings: true
+      }
     ],
-    'globals': {
-      Promise: true
-    },
-    'plugins': [
-        'react',
-        'prettier',
-        'import'
-    ],
-    'rules': {
-      'prettier/prettier': 'error',
-      'react/react-in-jsx-scope': 'off',
-      'no-unused-vars': [
-        'error',
-        {
-          argsIgnorePattern: '^_',
-          varsIgnorePattern: '^_',
-          ignoreRestSiblings: true
-        }
-      ],
-      'no-param-reassign': 'error'
-    }
+    'no-param-reassign': 'error'
+  }
 };
