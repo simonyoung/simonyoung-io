9dd46150d2319a2894cc4accda518540e597e2ab
Simon Young <simon.young@lego.com>
2020-01-27 22:02:49 +0000

    Add date formatting for post date

--- components/Post.js
+++ components/Post.js
@@ -1,12 +1,14 @@
 import { useRouter } from 'next/router';
 import matter from 'gray-matter';
 import ReactMarkdown from 'react-markdown';
+import Moment from 'react-moment';
 
 export default ({ title, data }) => {
   return (
     <div>
       <h1>{data.title}</h1>
       <h3>by {data.author}</h3>
+      <Moment date={data.date} format="dddd Do MMMM YYYY" />
     </div>
   );
 };
