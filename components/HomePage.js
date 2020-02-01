7fd2008131b5ac7ed315e539148fc0b767cc60b4
Simon Young <simon.young@lego.com>
2020-02-01 21:21:04 +0000

    Refactor HomePage.js

--- components/HomePage.js
+++ components/HomePage.js
@@ -1,7 +1,3 @@
-//const HelloWorld = () => <div>Hello world. Welcome to the site.</div>;
-
-//export default HelloWorld;
-
 import Link from 'next/link';
 
 const PostLink = props => (
@@ -11,17 +7,6 @@ const PostLink = props => (
     </Link>
   </li>
 );
-/*
-export default function Posts() {
-  return (
-    <>
-      <ul className="list">
-        <PostLink id="the-empire-strikes-back" />
-      </ul>
-    </>
-  );
-}
-*/
 
 export default ({ allBlogs, siteConfig }) => {
   console.log(allBlogs);
