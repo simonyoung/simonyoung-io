5e08cdbf0b5e6f4aabd5a0a4ccf881ee7daf84e1
Simon Young <simon.young@lego.com>
2020-02-01 22:20:27 +0000

    Add SSR support for styled-components

--- pages/_app.js
+++ pages/_app.js
@@ -1,14 +1,21 @@
 import React from 'react';
 import App from 'next/app';
 import Layout from '../components/Layout';
+import { ThemeProvider } from 'styled-components';
+
+const theme = {
+  primary: 'green'
+};
 
 class MyApp extends App {
   render() {
     const { Component, pageProps } = this.props;
     return (
-      <Layout>
-        <Component {...pageProps}></Component>
-      </Layout>
+      <ThemeProvider theme={theme}>
+        <Layout>
+          <Component {...pageProps}></Component>
+        </Layout>
+      </ThemeProvider>
     );
   }
 }
