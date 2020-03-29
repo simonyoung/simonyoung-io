d792c91fcd954633429114f159ec697a1a1acbf3
Simon Young <simon.young@lego.com>
2020-03-29 19:03:58 +0100

    Add dotenv

--- next.config.js
+++ next.config.js
@@ -1,5 +1,11 @@
 //next.config.js
+require('dotenv').config();
+
 module.exports = {
+  env: {
+    CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
+  },
   webpack: function(config) {
     config.module.rules.push({
       test: /\.md$/,
