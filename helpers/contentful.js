ae552fefa181b397c92405ebc386a6c4bb09ebd7
Simon Young <simon.young@lego.com>
2020-03-29 19:04:18 +0100

    Add Contentful helper

---
+++ helpers/contentful.js
@@ -0,0 +1,23 @@
+import { ContentfulClientApi, createClient, getEntries } from 'contentful';
+
+const client = createClient({
+  space: process.env.CONTENTFUL_SPACE_ID,
+  accessToken: process.env.CONTENTFUL_ACCESS_TOKEN
+});
+
+export const getContent = async content_type => {
+  const entries = await client.getEntries({
+    content_type: content_type
+  });
+  if (entries.items) return entries.items;
+  console.log(`Error getting Entries for ${contentType.name}.`);
+};
+
+export const getContentBySlug = async slug => {
+  const entry = await client.getEntries({
+    content_type: 'post',
+    'fields.url': slug
+  });
+  if (entry.items) return entry.items[0];
+  console.log(`Error getting Entries for ${contentType.name}.`);
+};
