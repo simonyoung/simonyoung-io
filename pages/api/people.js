899a1b0a331d4ae7542c7e095d69b9e0d7813c94
Simon Young <simon.young@lego.com>
2020-03-29 12:43:14 +0100

    Add sample API

---
+++ pages/api/people.js
@@ -0,0 +1,7 @@
+import { people } from '../../data/sample';
+
+export default (req, res) => {
+  res.statusCode = 200;
+  res.setHeader('Content-Type', 'application/json');
+  res.end(JSON.stringify(people));
+};
