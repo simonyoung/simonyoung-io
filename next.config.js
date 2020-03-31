cb325f2cbf61346059a24ff200bd510925decfba
Simon Young <simon.young@lego.com>
2020-03-31 17:45:57 +0100

    Update to use env var for base API URL

--- next.config.js
+++ next.config.js
@@ -4,7 +4,9 @@ require('dotenv').config();
 module.exports = {
   env: {
     CONTENTFUL_SPACE_ID: process.env.CONTENTFUL_SPACE_ID,
-    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN
+    CONTENTFUL_ACCESS_TOKEN: process.env.CONTENTFUL_ACCESS_TOKEN,
+    CONTENTFUL_BLOG_CONTENT_TYPE: process.env.CONTENTFUL_BLOG_CONTENT_TYPE,
+    NOW_URL: process.env.NOW_URL
   },
   webpack: function(config) {
     config.module.rules.push({
