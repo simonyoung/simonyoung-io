8c89942267baf1f3f9c8c706b98061780777b437
Simon Young <simon.young@lego.com>
2020-04-01 22:24:34 +0100

    Remove unneeded test API endpoints

--- pages/api/people.js
+++
@@ -1,7 +0,0 @@
-import { people } from '../../data/sample';
-
-export default (req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'application/json');
-  res.end(JSON.stringify(people));
-};
