32bf966ae289b83dc53b13674a677ff1ec169be0
Simon Young <simon.young@lego.com>
2020-04-04 12:00:45 +0100

    Updated dynamiuc page route

--- pages/[page].js
+++ pages/[page].js
@@ -1,23 +1,30 @@
+import { useRouter } from 'next/router';
 import { getContent, getPageBySlug } from '../helpers/contentful';
 import DynamicContent from '../components/DynamicContent';
 
 const DynamicContentPage = ({ siteTitle, data }) => {
+  const router = useRouter();
+
+  if (router.isFallback) {
+    return <div>Loading...</div>;
+  }
+
   return <DynamicContent title={siteTitle} data={data} />;
 };
 
 export async function getStaticPaths() {
   const posts = await getContent(process.env.CONTENTFUL_PAGE_CONTENT_TYPE);
-
+  console.log(posts);
   const paths = posts.map(post => ({
-    params: { slug: post.fields.url }
+    params: { page: post.fields.url }
   }));
 
-  return { paths, fallback: false };
+  return { paths, fallback: true };
 }
 
 export async function getStaticProps({ params }) {
   const siteConfig = await import(`../data/config.json`);
-  const data = await getPageBySlug(params.slug);
+  const data = await getPageBySlug(params.page);
 
   return {
     props: {
