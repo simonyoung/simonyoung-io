471e87832b936993f3400cf372cef16e9b12917c
Simon Young <simon.young@lego.com>
2020-05-16 17:37:14 +0100

    Additional absolute import fixes

--- src/components/Layout.js
+++ src/components/Layout.js
@@ -1,5 +1,5 @@
-import Header from './Header/Header';
-import Footer from './Footer/Footer';
+import Header from 'components/Header/Header';
+import Footer from 'components/Footer/Footer';
 import Head from 'next/head';
 
 const Layout = ({ children }) => {
