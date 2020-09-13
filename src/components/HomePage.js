c570da642ab4007b229d77f9f4771137b569e2db
Simon Young <simon.young@lego.com>
2020-09-13 21:56:36 +0100

    WIP

--- src/components/HomePage.js
+++ src/components/HomePage.js
@@ -1,5 +1,7 @@
 import Link from 'next/link';
 import Headline from 'components/Headline';
+import PostTitle from 'components/PostTitle';
+import PostMeta from 'components/PostMeta';
 import Moment from 'react-moment';
 import ReactMarkdown from 'react-markdown';
 
@@ -23,35 +25,9 @@ const HomePage = ({ allPosts, siteConfig }) => {
             >
               <div className="relative px-4 sm:px-6 lg:px-8">
                 <div className="text-lg max-w-prose mx-auto ">
-                  <h1 className="mt-2 mb-6 text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
-                    {blog.fields.post_title}
-                  </h1>
-                  <div className="w-max-content mx-auto">
-                    <p className="align-items inline-block px-2 py-0 text-base leading-6 ml-2 uppercase font-medium">
-                      By Simon Young
-                    </p>
-                    <p className="align-items inline-block px-2">
-                      <svg
-                        className="h-6 w-6 my-2 relative inline-block"
-                        xmlns="http://www.w3.org/2000/svg"
-                        fill="none"
-                        viewBox="0 0 24 24"
-                        stroke="currentColor"
-                      >
-                        <path
-                          strokeLinecap="round"
-                          strokeLinejoin="round"
-                          strokeWidth={2}
-                          d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
-                        />
-                      </svg>
-                      <Moment
-                        date={blog.fields.publish_date}
-                        format="DD MMM YYYY"
-                        className="text-base leading-6 ml-2 uppercase font-medium inline-block"
-                      />
-                    </p>
-                  </div>
+                  <PostTitle title={blog.fields.post_title} />
+                  <PostMeta date={blog.fields.publish_date} />
+
                 </div>
                 <div>
                   <img
@@ -61,14 +37,22 @@ const HomePage = ({ allPosts, siteConfig }) => {
                   />
                 </div>
                 <div className="prose prose-lg text-gray-500 mx-auto pb-10">
-                  <ReactMarkdown source={blog.fields.content_preview} />
-                  <p>
-                    <a>
-                      <span className="text-gray-600 hover:text-gray-800 underline">
-                        Read more
-                      </span>
-                    </a>
-                  </p>
+
+                <div>
+      {blog.fields.content_preview
+        ? <div>
+            <ReactMarkdown source={blog.fields.content_preview} />
+            <p>
+              <a>
+                <span className="text-gray-600 hover:text-gray-800 underline">
+                  Read more
+                </span>
+              </a>
+            </p>
+          </div>
+        : <ReactMarkdown source={blog.fields.post_content} />
+      }
+    </div>
                 </div>
                 <hr />
               </div>
---
+++ src/components/PostMeta.js
@@ -0,0 +1,35 @@
+import Moment from 'react-moment';
+
+const PostMeta = ({date}) => {
+  return (
+    <>
+    <div className="w-max-content mx-auto">
+                    <p className="align-items inline-block px-2 py-0 text-base leading-6 ml-2 uppercase font-medium">
+                      By Simon Young
+                    </p>
+                    <p className="align-items inline-block px-2">
+                      <svg
+                        className="h-6 w-6 my-2 relative inline-block"
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        stroke="currentColor"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth={2}
+                          d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                        />
+                      </svg>
+                      <Moment
+                        date={date}
+                        format="DD MMM YYYY"
+                        className="text-base leading-6 ml-2 uppercase font-medium inline-block"
+                      />
+                    </p>
+                  </div>
+    </>
+  );
+};
+export default PostMeta;
