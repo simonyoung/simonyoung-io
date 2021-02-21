0a33ede49878e39d71d18e8b554b1b2f9c505aa7
Simon Young <simon180@mac.com>
2021-02-21 10:41:10 +0000

    fix: Broken blog layout

--- src/components/Blog.tsx
+++ src/components/Blog.tsx
@@ -11,27 +11,25 @@ const Blog = ({ posts }: Props) => {
   const isPreview = true;
   return (
     <>
-      <div className="max-w-screen-xl mx-auto py-8 px-4">
-        <div className="relative max-w-lg mx-auto">
-          <div>
-            <h2 className="text-3xl text-center leading-9 tracking-tight font-extrabold text-logo-mid">
-              Blog
-            </h2>
-            <div className="mt-3">
-              <p className="text-base text-center leading-7 text-logo-light">
-                Full length articles on engineering management, technology and leadership.
-              </p>
-            </div>
+      <div className="max-w-screen-xl mx-auto relative overflow-hidden py-8 px-4">
+        <div>
+          <h2 className="text-3xl text-center leading-9 tracking-tight font-extrabold text-logo-mid">
+            Blog
+          </h2>
+          <div className="mt-3">
+            <p className="text-base text-center leading-7 text-logo-light">
+              Full length articles on engineering management, technology and leadership.
+            </p>
           </div>
-          <div className="mt-6 grid gap-16 border-t-2 border-gray-100 pt-10">
-            {posts.map((post: any, index: number) => {
-              return (
-                <div key={index}>
-                  <Post isPreview={isPreview} key={index} {...post} />
-                </div>
-              );
-            })}
-          </div>
+        </div>
+        <div className="mt-6 grid gap-16 border-t-2 border-solid border-logo-dark pt-10">
+          {posts.map((post: any, index: number) => {
+            return (
+              <div key={index}>
+                <Post isPreview={isPreview} key={index} {...post} />
+              </div>
+            );
+          })}
         </div>
       </div>
     </>
